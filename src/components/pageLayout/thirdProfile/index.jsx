699a9472332355515c0c8f931a493663453f1c40
JoaoKeuwe <joaov2872@hotmail.com>
2023-10-17 15:32:34 -0300

    feat:

--- src/components/pageLayout/thirdProfile/index.jsx
+++ src/components/pageLayout/thirdProfile/index.jsx
@@ -1,9 +1,17 @@
 import GenericProfile from '../genericProfile'
+import profile from '../../../assets/images/profile3.svg'
+import cta from '../../../assets/images/cta.svg'
 
 const ThirdProfile = () => {
-  return (
-      <GenericProfile />
-  )
+    return (
+        <GenericProfile
+            profile={profile}
+            description={'“ Conversion rates improved as a result of Nextly’ s work.The team is organized and has strong practices for development projects.Everyone is happy with the new platform.”'}
+            name={"Gustavo Paiva"}
+            job={'CEO at Descola'}
+            cta={cta}
+        />
+    )
 }
 
 export default ThirdProfile
