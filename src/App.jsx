9874175b74d96aa5854088492900fe89597876b2
JoaoKeuwe <joaov2872@hotmail.com>
2023-10-17 03:51:38 -0300

    feat: criando 3 seções principais

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import "./App.css";
-import MainPage from "../src/components/pageLayout/index";
+import MainPage from "./components/pageLayout/mainSection/index";
 import NavBar from "./components/navBar";
-
+import AboutUs from "./components/pageLayout/aboutUsSection";
+import VendooPage from "./components/pageLayout/vendooPage";
+import FirstProfile from "./components/pageLayout/firstProfile";
+import JobPixel from "./components/pageLayout/jobpixel";
 function App() {
+  const divStyle = {
+    background: "linear-gradient(288deg, #0038a7 1.5%, #004ba0 91.6%)",
+    height: "90vh",
+  };
+
   return (
     <>
-      <div style={{ background: 'blue', margin: 0 }}>
-
+      <div style={divStyle}>
         <NavBar />
-
         <MainPage />
       </div>
+      <AboutUs />
+      <VendooPage />
+      <FirstProfile />
+      <JobPixel />
     </>
   );
 }
